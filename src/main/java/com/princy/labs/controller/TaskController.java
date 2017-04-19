50431aafa7070c9f9336d79a9d04dfc104007966
Princy Razafimanantsoa <princy.raza@gmail.com>
2017-04-19 16:25:56 +0200

    Add hello endpoint

--- src/main/java/com/princy/labs/controller/TaskController.java
+++ src/main/java/com/princy/labs/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.princy.labs.controller;
 
 import com.princy.labs.model.Task;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,4 +13,8 @@ import java.util.List;
 @RestController
 public class TaskController {
 
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello "+name;
+    }
 }
